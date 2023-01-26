6ce96814436d69ffea06111242563d7db59f29dd
Ahmed Hesham <ahmedhesham.1652001@gmail.com>
2023-01-26 22:22:25 +0200

    Delete demo/src/main/java/com/ProducerConsumer/demo/AssemblyLine directory

--- demo/src/main/java/com/ProducerConsumer/demo/AssemblyLine/MyQueue.java
+++
@@ -1,8 +0,0 @@
-package com.ProducerConsumer.demo.AssemblyLine;
-
-public class MyQueue extends Shape {
-    MyQueue(double x, double y) {
-        super(new IdGenerator().creatId("queue"), x, y);
-    }
-
-}
